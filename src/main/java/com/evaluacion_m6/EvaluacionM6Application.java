9f0395e8b2b64f88014c56e563f1d0875b6939e7
anabanana <ana.vallejops18@gmail.com>
2024-06-21 22:09:51 -0400

    first commit

---
+++ src/main/java/com/evaluacion_m6/EvaluacionM6Application.java
@@ -0,0 +1,13 @@
+package com.evaluacion_m6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionM6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionM6Application.class, args);
+	}
+
+}
